2be56cf47ab424734ab4f4e134dca03968523b0c
TheVirus! <kingmouryarahul@gmail.com>
2017-11-10 13:49:00 +0530

    Update edit_boot.sh

--- mkzip/scripts/edit_boot.sh
+++ mkzip/scripts/edit_boot.sh
@@ -1,25 +1,36 @@
 #!/sbin/sh
 
+console=$(cat /tmp/console)
+[ "$console" ] || console=/proc/$$/fd/1
+
+print() {
+	echo "ui_print - $1" > $console
+	echo
+}
+
 TMP="/tmp/kernel"
-zImage="$TMP/zimg/zImage"
-BUSYBOX="/$TMP/busybox"
-LZ4="$TMP/lz4"
-BOOT_DEV="$(cat $TMP/bootdev)"
-RD_ADDR="$(cat $TMP/rd_addr)"
+B="$TMP/scripts/bootimg"
+zImage="$TMP/scripts/zimg/zImage"
+DTB="$TMP/scripts/DTB/default.dtb"
+BUSYBOX="$TMP/scripts/busybox"
+LZ4="$TMP/scripts/lz4"
+LZMA="$TMP/scripts/lzma"
+BOOT_DEV="$(cat $TMP/scripts/bootdev)"
+RD_ADDR="$(cat $TMP/scripts/rd_addr)"
 Kr="$TMP/zImage"
 
 CMPR_GZIP=0
 CMPR_LZ4=1
 
 if [ ! -e "$BOOT_DEV" ]; then
-    echo "BOOT_DEV \"$BOOT_DEV\" does not exist!"
+    print "- BOOT_DEV \"$BOOT_DEV\" does not exist!"
     return 1
-fi
+ fi
 
 dd if=$BOOT_DEV of=$TMP/boot.img
-$TMP/bbootimg -x $TMP/boot.img $TMP/bootimg.cfg $TMP/zImage $TMP/initrd.img $TMP/second.img $TMP/dtb.img
+$B -x $TMP/boot.img $TMP/bootimg.cfg $TMP/zImage $TMP/initrd.img $TMP/second.img $TMP/dtb.img
 if [ ! -f $TMP/zImage ] ; then
-    echo "Failed to extract boot.img"
+    print "- Failed to extract boot.img"
     return 1
 fi
 
@@ -39,18 +50,19 @@ case "$magic" in
         rd_cmpr=CMPR_LZ4;
         ;;
     *)
-        echo "invalid ramdisk magic $magic"
+        print "- invalid ramdisk magic $magic"
         ;;
 esac
 
 if [ rd_cmpr == -1 ] || [ ! -f $TMP/boot/init ] ; then
-    echo "Failed to extract ramdisk!"
+    print "- Failed to extract ramdisk!"
     return 1
     
 fi
 
 #Updating kernel
 
+print "- Updating kernel... "
 if [ -e $Kr ]; then
     chmod 777 $Kr
       rm $Kr
@@ -58,7 +70,19 @@ if [ -e $Kr ]; then
       
 fi
 
+#Updating dtb
+
+print "- Updating dtb... "
+if [ -f $DTB ]; then
+    chmod 777 $TMP/dtb.img
+      rm -r $TMP/dtb.img
+      cp $DTB $TMP/default.dtb
+      
+fi
+
 # repack the image again
+
+print "- repacking the image again ... "
 cd $TMP/boot
 
 case $rd_cmpr in
@@ -72,24 +96,26 @@ esac
 
 echo "bootsize = 0x0" >> $TMP/bootimg.cfg
 if [ -n "$RD_ADDR" ]; then
-    echo "Using ramdisk addr $RD_ADDR"
+    print "- Using ramdisk addr $RD_ADDR"
     echo "ramdiskaddr = $RD_ADDR" >> $TMP/bootimg.cfg
 fi
 
 cd $TMP
 
 dtb_cmd=""
-if [ -f "dtb.img" ]; then
+if [ -f "default.dtb" ]; then
+   dtb_cmd="-d default.dtb"
+ else
     dtb_cmd="-d dtb.img"
 fi
 
-$TMP/bbootimg --create newboot.img -f bootimg.cfg -k zImage -r initrd.img $dtb_cmd
+$B --create kernel.img -f bootimg.cfg -k zImage -r initrd.img $dtb_cmd
 
-if [ ! -e "$TMP/newboot.img" ] ; then
-    echo "Failed to inject boot.img!"
+if [ ! -e "$TMP/kernel.img" ] ; then
+    print "- Failed to inject boot.img!"
     return 1
 fi
 
-echo "Writing new boot.img..."
-dd bs=4096 if=$TMP/newboot.img of=$BOOT_DEV
+print "- Writing new boot.img..."
+dd bs=4096 if=$TMP/kernel.img of=$BOOT_DEV
 return $?
